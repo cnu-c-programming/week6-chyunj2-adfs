69787e8b0028c9f6b81fa96ab2e5e1ada553e100
chyunj2-adfs <chyunj2@gmail.com>
2026-04-16 19:26:04 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
     int target_array[2][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
     int (*array_pointer)[5] = target_array;
     int a = 10, b = 20, c = 30, d = 40, e = 50;
     int * pointer_array[5] = {&a, &b, &c, &d, &e};
     
-    printf("size of (array_pointer): %zu\n", sizeof(array_pointer));
-    printf("size of (pointer_array): %zu\n", sizeof(pointer_array));
+    printf("sizeof(array_pointer): %zu\n", sizeof(array_pointer));
+    printf("sizeof(pointer_array): %zu\n", sizeof(pointer_array));
     printf("\n");
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 4; i++)
+    {
         printf("%p\n", (void*)(array_pointer + i));
     }
     printf("\n");
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 4; i++)
+    {
         printf("%p\n", (void*)(pointer_array + i));
     }
     return 0;
